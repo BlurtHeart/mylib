c46441ef643393ee96ca34ebab3f6ae32cd74b55
blurtheart <fuhongbofhb@163.com>
2017-08-27 17:15:19 +0800

    context usage

--- context/context.go
+++ context/context.go
@@ -1,7 +1,28 @@
 // implement standard library's context
 
-package context
+package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
 
-var empty = new(context.Context)
+func sleepAndTalk(ctx context.Context, duration time.Duration, data string) {
+	select {
+	case <-time.After(duration):
+		fmt.Println(data)
+	case <-ctx.Done():
+		log.Print(ctx.Err())
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*6)
+	defer cancel()
+
+	sleepAndTalk(ctx, time.Second*5, "hello")
+}
