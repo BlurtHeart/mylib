b8ddc7945fe47cf711db6fa79372c4ab5960c4d2
blurtheart <fuhongbofhb@163.com>
2017-08-28 23:47:59 +0800

    continue implement context

--- context/context.go
+++ context/context.go
@@ -1,28 +1,90 @@
 // implement standard library's context
 
-package main
+package context
 
 import (
-	"context"
-	"fmt"
-	"log"
+	"errors"
 	"time"
 )
 
-func sleepAndTalk(ctx context.Context, duration time.Duration, data string) {
-	select {
-	case <-time.After(duration):
-		fmt.Println(data)
-	case <-ctx.Done():
-		log.Print(ctx.Err())
+type Context interface {
+	Deadline() (deadline time.Time, ok bool)
+	Done() <-chan struct{}
+	Err() error
+	Value(key interface{}) interface{}
+}
+
+type emptyCtx int
+
+func (emptyCtx) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (emptyCtx) Done() <-chan struct{} {
+	return nil
+}
+
+func (emptyCtx) Err() error {
+	return nil
+}
+
+func (emptyCtx) Value(key interface{}) interface{} {
+	return nil
+}
+
+func (ctx *emptyCtx) String() string {
+	switch ctx {
+	case background:
+		return "context.Background"
+	case todo:
+		return "context.TODO"
+	}
+	return "Unkown empty context"
+}
+
+var (
+	background = new(emptyCtx)
+	todo       = new(emptyCtx)
+)
+
+func Background() Context {
+	return background
+}
+
+func TODO() Context {
+	return todo
+}
+
+type cancelFunc func()
+
+var Canceled = errors.New("context canceled")
+
+func WithCancel(parent Context) (Context, cancelFunc) {
+	c := newCancelCtx(parent)
+	return c, func() { c.cancel(true, Canceled) }
+}
+
+func newCancelCtx(parent Context) *CancelContext {
+	return &CancelContext{
+		Context: parent,
+		done:    make(chan struct{}),
 	}
 }
 
-func main() {
-	ctx := context.Background()
+type CancelContext struct {
+	Context
+	done chan struct{}
+	err  error
+}
 
-	ctx, cancel := context.WithTimeout(ctx, time.Second*6)
-	defer cancel()
+func (cctx *CancelContext) String() string {
+	return "context.Background.WithCancel"
+}
 
-	sleepAndTalk(ctx, time.Second*5, "hello")
+func (cctx *CancelContext) Done() <-chan struct{} {
+	return cctx.done
+}
+
+func (cctx *CancelContext) cancel(removeFromParent bool, err error) {
+	return
 }
